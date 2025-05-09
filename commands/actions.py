ba0cd4515501e727a2fa26a62a7afdea46a48c27
jakegodsall <jake.edward.godsall@gmail.com>
2025-05-09 21:28:39 +0100

    wip

--- commands/actions.py
+++ commands/actions.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from rich.console import Console
 from rich.table import Table
 
+from utils.reppd_client import ReppdClient
 from utils.config import configure_flow
 from utils.exceptions import ConfigurationError
 
@@ -16,7 +17,7 @@ reppd_client = ReppdClient()
 def list():
     """"""
     try:
-        actions = reppd_client.get_actions_list()
+        actions = reppd_client.get_action_list()
 
         table = Table("id", "title", "desciption", "status", "start_date")
         for a in actions:
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from commands.config import config_app
 from commands.init import init_app
 from commands.competency import competency_app
+from commands.actions import action_app
 
 load_dotenv()
 
@@ -11,6 +12,7 @@ app = typer.Typer()
 
 app.add_typer(init_app, name="init")
 app.add_typer(competency_app, name="competency")
+app.add_typer(action_app, name="action")
 app.add_typer(config_app, name="config")
 
 if __name__ == "__main__":
